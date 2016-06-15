49c298d6b3302cc2c289483e19b7299ae4eb9655
alem972 <melanie.gourpil@gmail.com>
2016-06-16 01:33:48 +0200

    edit of participant

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,17 +1,40 @@
 class ParticipantsController < ApplicationController
-  def create
-    @participant = Participant.new(sondage_params)
-    @participant.save
+  def new
+    participant = Participant.new
   end
+  def create
+    @participant = Participant.new(participant_params)
+    if @participant.save
+    redirect_to participants_path, methode: :get
+    else
+    render 'new'
+    end
+  end
+  
   def index
-    @participants = Participant.all  
+    @participants = Participant.all
   end
 
+  def edit
+    @participant = Participant.find(params[:id])
+  end
+
+  def update
+    @participant = Participant.find(params[:id])
+    if @participant.update(participant_params)
+      redirect_to partcipant_path @participant
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    
+  end
 
   private
   def participant_params
     params.require(:participant)
-    .permit(:name)
   end
 
 end
